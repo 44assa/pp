0ad9278ed5b7c20a7d3f9ae237f0774afc8b486c
assa <mleon_2005@mail.ru>
2025-04-06 10:46:02 +0300

    3

--- PP/main/urls.py
+++ PP/main/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main),
-    path('vacancy', views.vacancy)
+    path('', views.main, name='home'),
+    path('vacancy', views.vacancy, name='vacancy'),
+    path('firstVacancy', views.firstVacancy, name='firstVacancy'),
+    path('secondVacancy', views.secondVacancy, name='secondVacancy'),
+    path('application', views.application, name='application'),
+    path('myApplication', views.myApplication, name='myApplication'),
+    path('entrance', views.entrance, name='entrance'),
+    path('registration', views.registration, name='registration'),
+    path('profile', views.profile, name='profile')
 ]
--- PP/main/views.py
+++ PP/main/views.py
@@ -5,3 +5,24 @@ def main(request):
 
 def vacancy(request):
     return render(request, 'main/vacancy.html')
+
+def firstVacancy(request):
+    return render(request, 'main/firstVacancy.html')
+
+def secondVacancy(request):
+    return render(request, 'main/secondVacancy.html')
+
+def application(request):
+    return render(request, 'main/application.html')
+
+def myApplication(request):
+    return render(request, 'main/myApplication.html')
+
+def entrance(request):
+    return render(request, 'main/entrance.html')
+
+def registration(request):
+    return render(request, 'main/registration.html')
+
+def profile(request):
+    return render(request, 'main/profile.html')
